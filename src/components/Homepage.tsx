f87e979cc2f4115d67b846125f8ced68de136c68
Eliya Vahl <eliya@in3d-tech.com>
2025-01-16 15:06:11 +0200

    unused-variable

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 // import { Questionaire } from "./questionnaire/Questionnaire";
 // import { PdfViewer } from "./common/PdfViewer";
 // import { Item } from "./Item";
