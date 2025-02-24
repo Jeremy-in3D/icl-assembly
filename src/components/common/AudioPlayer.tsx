42f5a86a9f5b76caa6acf0a43a1241a0a67893e2
Eliya Vahl <eliya@in3d-tech.com>
2025-02-24 15:01:20 +0200

    chinese-txt-speech

--- src/components/common/AudioPlayer.tsx
+++ src/components/common/AudioPlayer.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useAppContext } from "../../context/appContext";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import { useTranslation } from "react-i18next";
 
 const getAudioSrc = (currentStep: any) => {
   if (currentStep.item === 4) {
@@ -59,6 +60,10 @@ export const AudioPlayer = ({ currentStep }: any) => {
   const audioRef: any = useRef(null);
   const [audioSrc, setAudioSrc] = useState("");
 
+  const { i18n } = useTranslation();
+
+  const language = i18n.language || "en";
+
   const {
     isPlayingAudio,
     setIsPlayingAudio,
@@ -69,7 +74,9 @@ export const AudioPlayer = ({ currentStep }: any) => {
   useEffect(() => {
     const srcToPlay = getAudioSrc(currentStep);
     // setAudioSrc(`assets/audio/item-${srcToPlay}`);
-    setAudioSrc(`assets/audio/item-${srcToPlay}?v=${Date.now()}`);
+    language == "en"
+      ? setAudioSrc(`assets/audio/en/item-${srcToPlay}?v=${Date.now()}`)
+      : setAudioSrc(`assets/audio/zh/item-${srcToPlay}-zh?v=${Date.now()}`);
   }, [currentStep]);
 
   useEffect(() => {
